2d9d132ab4f697663123efcaf58e63e95ef0f47e
Femi <A442UF@Femi-Elice>
2020-11-04 12:11:40 +0700

    create packages main

---
+++ SYSTEM_RESTORANT/src/Controller/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.GUIMenu.Screen;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.ImageObserver;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author A442UF
+ */
+public class Main {  
+    public static void main(String[] args){
+        new Screen();
+    }
+}
