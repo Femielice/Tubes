6da1307b0f67582357e268f150c1ee0e4e03cc94
William Surjana <williamsurjana2013@gmail.com>
2020-11-13 21:07:53 +0700

    Create Method

---
+++ Tubes/src/Controller/method.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author SS
+ */
+public class method{
+    public method(){
+    }
+    
+    public double pendapatan(){
+        int harga= 10000;
+        return harga;
+    }
+  
+}
